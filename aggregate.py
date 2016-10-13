4abb12b5a677f2b53f96a5bca37075ea0a16f77e
Daisuke Sugiyama <dsugiyama@outlook.jp>
2016-10-13 15:39:20 +0900

    modify execution configuration for KNL

--- aggregate.py
+++ aggregate.py
@@ -4,13 +4,11 @@ import re
 import itertools
 
 niter = int(sys.argv[1])
-mic = sys.argv[2]
-cmd = " ".join(sys.argv[4:])
+cmd = " ".join(sys.argv[3:])
 
 iter_cmd = "for i in `seq %d`; do %s; done" % (niter, cmd)
-ssh_cmd = "ssh %s -- '. .profile; cd tmp; %s'" % (mic, iter_cmd)
 
-output = commands.getoutput(ssh_cmd)
+output = commands.getoutput(iter_cmd)
 matches = re.finditer(r"(.+) overhead = ([0-9\.]+)", output)
 tuples = [(m.group(1), float(m.group(2))) for m in matches]
 keyfunc = lambda t: t[0]
--- run.sh
+++ run.sh
@@ -1,22 +1,22 @@
-nth="60 240"
+nth="64 256"
 
 bench=$1
 niter=$2
-mic=$3
+part=$3
 
 for n in $nth; do
     echo "Intel"
-    python aggregate.py $niter $mic -- \
+    srun -p $part -- python aggregate.py $niter -- \
         OMP_NUM_THREADS=$n ./$bench-intel
     echo
 
-    echo "Omni (Pthread)"
-    python aggregate.py $niter $mic -- \
-        OMP_NUM_THREADS=$n ./$bench-omni
+    echo "Omni-Argobots"
+    srun -p $part -- python aggregate.py $niter -- \
+        OMP_NUM_THREADS=$n OMPC_NUM_PROCS=$n ./$bench-abt
     echo
 
-    echo "Omni (Argobots)"
-    python aggregate.py $niter $mic -- \
-        OMPC_NUM_PROCS=$n OMPC_NUM_FORK=$n ./$bench-abt
+    echo "BOLT"
+    srun -p $part -- python aggregate.py $niter -- \
+        OMP_NUM_THREADS=$n OMP_THREAD_LIMIT=$n ./$bench-bolt
     echo
 done
