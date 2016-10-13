5d3f6d1fd6bdfa68193fad6cbb0e278100eaf3cf
Daisuke Sugiyama <dsugiyama@outlook.jp>
2016-10-13 15:20:12 +0900

    modify build for KNL

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ if (($# != 1)); then
     exit 1
 fi
 
-for defs in Makefile.defs.{intel,omni,abt}; do
+for defs in Makefile.defs.{intel,abt,bolt}; do
     cp $defs Makefile.defs
     make $1
     mv $1 $1-${defs##*.}
